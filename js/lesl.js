80d3cc3fa45c014603f5f031234a747933d4b115
leslstrauss <leslie.strauss@gmail.com>
2014-11-30 04:17:56 -0500

    Figuring out js issues.

--- js/lesl.js
+++ js/lesl.js
@@ -1,18 +1,12 @@
-console.log('working');
-
 $(function() {
     $.ajax({
         type: "GET",
         url: "https://api.flickr.com/services/rest/?method=flickr.interestingness.getList&api_key=b944cc8b49d1fefb91be5009980232c0&format=json&nojsoncallback=1&auth_token=72157647854321898-cf20a8088a540415&api_sig=20332eae5cb39101438ea4d92c4b5323",
         dataType: "json",
         success: function (json) {
-          // console.log json object
-          // iterate through the image objects
-          // find the image objects from json
           var photos = json.photos.photo;
+          console.log(json);
           $.each(photos, function(key, photo) {
-          // add images to the body
-          // figure out real image source
             var farmID = photo.farm;
             var serverID = photo.server;
             var id = photo.id;
@@ -23,10 +17,13 @@ $(function() {
               id + '_' +
               secret + '.jpg';
 
-            $('<img>') // create an image
+            $('<img>')
               .attr('src', imageURL)
               .attr('alt', 'Image')
-              .appendTo('img'); // add to body
+              .addClass("grid-wrap")
+              .addClass("grid")
+
+              .appendTo('grid-gallery');
           });
 
         },
